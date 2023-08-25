f94cd1faca3d7f6163833b0662681fb3c606640a
Inga Gorbacheva <kardinal1985@list.ru>
2023-08-25 06:45:41 +0300

    Добавили папки 18,20

---
+++ Task20/Program.cs
@@ -0,0 +1,22 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+double FindDistance(int xA, int yA, int xB, int yB)
+{
+    return Math.Sqrt(Math.Pow(xA - xB, 2) + Math.Pow(yA - yB, 2));
+}
+
+Console.WriteLine("Введите координаты первой точки");
+Console.Write("X: ");
+int xCoordinateA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int yCoordinateA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки");
+Console.Write("X: ");
+int xCoordinateB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int yCoordinateB = Convert.ToInt32(Console.ReadLine());
+
+double result = FindDistance(xCoordinateA, yCoordinateA, xCoordinateB, yCoordinateB);
+Console.WriteLine($"Расстояние между точками -> {Math.Round(result, 2, MidpointRounding.ToZero)}");
