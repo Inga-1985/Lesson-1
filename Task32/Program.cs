9e0b72e0a0429a5a69e679a28f4f49e3391855cc
Inga Gorbacheva <kardinal1985@list.ru>
2023-09-01 16:42:43 +0300

    Создали папку Task 32

--- Task32/Program.cs
+++ Task32/Program.cs
@@ -1,43 +1,7 @@
 ﻿
-/ 32.Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот. [-4, -8, 8, 2]-> [4, 8, -8, -2]
+// / 32.Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот. [-4, -8, 8, 2]-> [4, 8, -8, -2]
 
 int[] CreateArrayRndInt(int size, int min, int max)
-{
-    int[] arr = new int[size];
-    Random rnd = new Random();
-
-    for (int i = 0; i < arr.Length; i++)
-    {
-        arr[i] = rnd.Next(min, max + 1);
-    }
-    return arr;
-}
-
-void InversArray(int[] arr)
-{
-    for (int i = 0; i < arr.Length; i++)
-    {
-        arr[i] *= -1;
-    }
-}
-
-void PrintArray(int[] arr)
-{
-    Console.Write("[");
-    for (int i = 0; i < arr.Length; i++)
-    {
-        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
-        else Console.Write($"{arr[i]}");
-    }
-    Console.Write("]");
-}
-
-int[] array = CreateArrayRndInt(4, -8, 8);
-PrintArray(array);
-Console.WriteLine();
-InversArray(array);
-PrintArray(array);
-Егор Дёмин int[] CreateArrayRndInt(int size, int min, int max)
 {
     int[] arr = new int[size];
     Random rnd = new Random();
@@ -67,7 +31,7 @@ void PrintArray(int[] arr)
         if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
         else Console.Write($"{arr[i]}");
     }
-    Console.Write("]");
+    Console.Write("] ");
 }
 
 Console.Write("Введите число: ");
